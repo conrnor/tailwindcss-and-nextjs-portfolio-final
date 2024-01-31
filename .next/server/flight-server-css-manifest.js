1d153315c329647a8f356291dd4c739ca7c987f7
conrnor <ellisc883@gmail.com>
2024-01-31 20:30:13 +0000

    first commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/connorellis/Desktop/tailwindcss-and-nextjs-portfolio-final/app/page": [
+      "/Users/connorellis/Desktop/tailwindcss-and-nextjs-portfolio-final/styles/globals.css"
+    ]
+  },
+  "/Users/connorellis/Desktop/tailwindcss-and-nextjs-portfolio-final/app/layout.tsx": [
+    "/Users/connorellis/Desktop/tailwindcss-and-nextjs-portfolio-final/styles/globals.css"
+  ]
+}
---
+++ .next/static/webpack/app/page.ca4c8e5a095a3e4c.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fconnorellis%2FDesktop%2Ftailwindcss-and-nextjs-portfolio-final%2Fcomponents%2FHeroSection.tsx&server=false!":
+/*!************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fconnorellis%2FDesktop%2Ftailwindcss-and-nextjs-portfolio-final%2Fcomponents%2FHeroSection.tsx&server=false! ***!
+  \************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./components/HeroSection.tsx */ \"(app-client)/./components/HeroSection.tsx\"))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPSUyRlVzZXJzJTJGY29ubm9yZWxsaXMlMkZEZXNrdG9wJTJGdGFpbHdpbmRjc3MtYW5kLW5leHRqcy1wb3J0Zm9saW8tZmluYWwlMkZjb21wb25lbnRzJTJGSGVyb1NlY3Rpb24udHN4JnNlcnZlcj1mYWxzZSEuanMiLCJtYXBwaW5ncyI6IkFBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz9hNGUwIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiICovIFwiL1VzZXJzL2Nvbm5vcmVsbGlzL0Rlc2t0b3AvdGFpbHdpbmRjc3MtYW5kLW5leHRqcy1wb3J0Zm9saW8tZmluYWwvY29tcG9uZW50cy9IZXJvU2VjdGlvbi50c3hcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fconnorellis%2FDesktop%2Ftailwindcss-and-nextjs-portfolio-final%2Fcomponents%2FHeroSection.tsx&server=false!\n"));
+
+/***/ })
+
+});
